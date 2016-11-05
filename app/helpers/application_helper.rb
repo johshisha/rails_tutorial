3d15a1c2ade4e5a6b903191fad4fc0c5f26d8647
johshisha <johshisha@gmail.com>
2016-11-05 17:13:34 +0900

    make full_title method to set title per page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  #return full title per page
+  def full_title(page_title = '')
+    base_title = 'Ruby on Rails Tutorial'
+    if page_title.empty?
+      base_title
+    else
+      page_title + '|' + base_title
+    end
+  end
 end
