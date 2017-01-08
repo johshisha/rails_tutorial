392fb1ce0d8b1a6c5a7139e51363a28271437551
johshisha <johshisha@gmail.com>
2017-01-08 18:56:11 +0900

    generate AccountActivations controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
+  
+  resources :account_activations, only: [:edit]
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
