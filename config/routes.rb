bd89887eb2c42633ec574dd95b13503c8cb3161f
johshisha <johshisha@gmail.com>
2017-01-09 16:42:12 +0900

    add function for activation account

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
   
-  resources :account_activations, only: [:edit]
+  resources :account_activations, only: [:index, :edit]
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
