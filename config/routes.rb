25b562d68b584206d22334e574607bfc658dec5d
johshisha <johshisha@gmail.com>
2017-01-10 02:24:59 +0900

    sent reset password mail and create view(12.3.1)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   root 'static_pages#home'
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
@@ -11,7 +15,8 @@ Rails.application.routes.draw do
   delete 'logout' => 'sessions#destroy'
   
   resources :account_activations, only: [:index, :edit]
-
+  
+  resources :password_resets, only: [:new, :create, :edit, :update]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
