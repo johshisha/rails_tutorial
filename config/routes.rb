780a9dec497f4092e125256eacf5aeb2486ea1da
johshisha <johshisha@gmail.com>
2017-01-26 21:13:16 +0900

    create post_page and post microposts

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:index, :edit]
   
   resources :password_resets, only: [:new, :create, :edit, :update]
+  
+  resources :microposts, only: [:create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
