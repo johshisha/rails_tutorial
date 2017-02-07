c08b8fcba8d938f10a791cb63be7e3fa5d8207f2
johshisha <johshisha@gmail.com>
2017-02-08 02:35:27 +0900

    test relationships

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,4 @@
+require File.expand_path('../boot', __FILE__)
 require_relative 'boot'
 
 require 'rails/all'
@@ -9,8 +10,6 @@ Bundler.require(*Rails.groups)
 module SampleApp
   class Application < Rails::Application
     config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')] # ref http://qiita.com/h5y1m141@github/items/97e1941086c7f9f229ac
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
